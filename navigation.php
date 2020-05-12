bc1ac0aa47c1323c042b4ce2b97ab830fa1f8117
olaf majoor <323666@student.mboutrecht.nl>
2020-05-12 09:30:17 +0200

    laravel set up

--- navigation.php
+++
@@ -1,9 +0,0 @@
-<section class="content">
-    <?php 
-       if(isset($_GET["content"])){
-           include("./" .$_GET["content"] . ".php");
-       }else{
-           include("./home.php");
-       }
-       ?>
-</section>
