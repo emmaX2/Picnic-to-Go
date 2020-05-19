3f301846a1ceadc6fc0aae0be5da12f205937b0c
Emma van Breugel <320711@student.mboutrecht.nl>
2020-05-19 09:53:52 +0200

    .

--- navbar.php
+++ navbar.php
@@ -7,7 +7,7 @@
   </div>
   <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
   <li class="nav-item">
-        <a class="nav-link" href="#">Winkelmand</a>
+        <a class="nav-link" href="#">Winkelwagen</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Gebruiker</a>
