a2e320d4e77f1802a4cd656a67e5eab31f917470
Noah Nap <321833@student.mboutrecht.nl>
2020-05-27 10:14:02 +0200

    added placeholder to carousel

--- home.php
+++ home.php
@@ -6,13 +6,13 @@
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="./img/hyrule.png" alt="First slide">
+      <img class="d-block w-100" src="https://via.placeholder.com/500x500" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="./img/hyrule.png" alt="Second slide">
+      <img class="d-block w-100" src="https://via.placeholder.com/150" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="./img/hyrule.png" alt="Third slide">
+      <img class="d-block w-100" src="https://via.placeholder.com/150" alt="Third slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
