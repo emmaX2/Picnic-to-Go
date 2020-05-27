a1bb8bddfe168bbe85d1451d10c8484190440bf8
olaf majoor <323666@student.mboutrecht.nl>
2020-05-27 10:03:46 +0200

    changes navbar added register

---
+++ register.php
@@ -0,0 +1,67 @@
+
+    <h4>Voer hier uw persoonlijke gegevens in</h4>
+    <br />
+
+
+<form action="">
+        <div class="form-row">
+            <div class="form-group col-md-2">
+                <label for="voornaam">Voornaam</label>
+                <input type="text" class="form-control" name="firstname" placeholder="Voornaam" required />
+            </div>
+            <div class="form-group col-md-2">
+                <label for="tussenvoegsel">Tussenvoegsel</label>
+                <input type="text" class="form-control" name="infix" placeholder="Tussenvoegsel" />
+            </div>
+            <div class="form-group col-md-2">
+                <label for="achternaam">Achternaam</label>
+                <input type="text" class="form-control" name="lastname" placeholder="Achternaam" required />
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-2">
+                <label for="huisnr">Huisnummer(+toevoeging) </label>
+                <input type="text" class="form-control" name="huisnr" placeholder="Huisnummer" required />
+            </div>
+            <div class="form-group col-md-2">
+                <label for="postcode">Postcode</label>
+                <input type="text" class="form-control" name="postcode" placeholder="Postcode" required />
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-2">
+                <label for="straat">Straatnaam</label>
+                <input type="text" class="form-control" name="straat" placeholder="straatnaam" required />
+            </div>
+            <div class="form-group col-md-2">
+                <label for="stad">Stad</label>
+                <input type="text" class="form-control" name="city" placeholder="Stad" required />
+            </div>
+            <div class="form-group col-md-2">
+                <label for="land">Land</label>
+                <input type="text" class="form-control" name="land" placeholder="Land" required />
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-2">
+                <label for="stad">Telefoonnummer</label>
+                <input type="text" class="form-control" name="phoneNumber" placeholder="Telefoonnummer" required />
+            </div>
+        </div>
+
+        <h4>Inloggegevens</h4>
+        <br />
+
+        <h5>U krijgt een e-mail op dit account waar u uw wachtwoord kan instellen</h5>
+
+        <div class="form-row">
+            <div class="form-group col-md-3">
+                <label for="stad">E-mail</label>
+                <input type="text" class="form-control" name="email" placeholder="E-mail" required />
+            </div>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Registreer</button>
+    </form>
+</div>
+   
