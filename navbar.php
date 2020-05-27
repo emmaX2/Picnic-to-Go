a7b5311b7fb4dfd09f0f78c873029acdedc7fbef
Emma van Breugel <320711@student.mboutrecht.nl>
2020-05-27 09:58:29 +0200

    product

--- navbar.php
+++ navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
-  <a class="navbar-brand" href="#" id="logo">LOGO</a>
+  <a class="navbar-brand" href="./index.php?content=home" id="logo">LOGO</a>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn my-2 my-sm-0" type="submit">Search</button>
@@ -25,7 +25,7 @@
           Alle merken
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">IDK</a>
+          <a class="dropdown-item" href="./index.php?content=product">Brood</a>
           <a class="dropdown-item" href="#">wat</a>
           <a class="dropdown-item" href="#">je</a>
           <a class="dropdown-item" href="#">hier</a>
@@ -39,24 +39,6 @@
         </div>
       </li>
       <li class="nav-item">
-<<<<<<< HEAD
-        <a class="nav-link" href="#">Groente</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Fruit</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="">Brood</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Vlees</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Vis</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Diepvries</a>
-=======
         <a class="nav-link" href="./index.php?content=vlees">Vlees</a>
       </li>
       <li class="nav-item">
@@ -70,7 +52,6 @@
       </li>
       <li class="nav-item">
         <a class="nav-link" href="./index.php?content=vriezer">Vriezer</a>
->>>>>>> 1d02923ab22e72493e08563f292928d9c6b5642a
       </li>
     </ul>
     
