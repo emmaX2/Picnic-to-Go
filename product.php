bc91f1389702b991bc6878c39892f375f81ee104
olaf majoor <323666@student.mboutrecht.nl>
2020-05-27 11:15:33 +0200

    Update product.php

--- product.php
+++ product.php
@@ -12,20 +12,20 @@
 
 <?php
 
-  // require_once("./connect_db.php");
+   require_once("./connect_db.php");
   
-  // $sql = "SELECT * FROM product ";
-  // $result = $conn->query($sql);
+  $sql = "SELECT * FROM product";
+  $result = $conn->query($sql);
 
-  // if (isset($_SESSION["id"])){
-  //   if($result->num_rows > 0){
-  //     while($row = $result->fetch_assoc()){
-  //       echo "<div class='row'><div class='col-6'>";
-  //       echo "<p> Productnaam: " . $row["Productnaam"] . "</p>";
-  //       echo "<p> Productomschrijving: " . $row["Productomschrijving"] . "</p>";
-  //       echo "<p> Productprijs: " . $row["Productprijs"] . "</p>";
-  //       echo "<img src='./img/" . $row["Productnaam"] . "/1.jpg' class='card-img-top' alt='afbeelding product'>";
-  //     }
-  //   }
-  // }
+  
+    if($result->num_rows > 0){
+      while($row = $result->fetch_assoc()){
+        echo "<div class='row'><div class='col-6'>";
+        echo "<p> Productnaam: " . $row["productnaam"] . "</p>";
+        echo "<p> Productomschrijving: " . $row["productomschrijving"] . "</p>";
+        echo "<p> Productprijs: " . $row["productprijs"] . "</p>";
+        echo "<img src='./img/" . $row["productfoto"] . "/1.jpg' class='card-img-top' alt='afbeelding product'>";
+      }
+    }
+  
 ?>
