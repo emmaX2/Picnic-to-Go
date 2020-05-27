3a1b209f680d1379ffb255c3b3c3d77d3cf1b47c
olaf majoor <323666@student.mboutrecht.nl>
2020-05-27 10:54:19 +0200

    nav bar 2nd button weg

--- navbar.php
+++ navbar.php
@@ -66,8 +66,3 @@
       
 </ul>
 </nav>
-<nav class="navbar navbar-expand-lg navbar-light ">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-</button>
-</nav>
