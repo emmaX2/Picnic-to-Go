ef642deb34758d00bd806e91b2a95a69ebe702aa
olaf majoor <323666@student.mboutrecht.nl>
2020-06-02 09:46:12 +0200

    Update product.php

--- product.php
+++ product.php
@@ -1,11 +1,4 @@
-<div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Bruin brood</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+
 
 
 
@@ -21,11 +14,23 @@
   
     if($result->num_rows > 0){
       while($row = $result->fetch_assoc()){
-        echo "<div class='row'><div class='col-6'>";
-        echo "<p> Productnaam: " . $row["productnaam"] . "</p>";
-        echo "<p> Productomschrijving: " . $row["productomschrijving"] . "</p>";
-        echo "<p> Productprijs: " . $row["productprijs"] . "</p>";
-        echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['productfoto'] ).'"/>';
+
+
+
+        echo'<div class="card" style="width: 18rem;">';
+  echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['productfoto'] ).'"' . ' class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">'. $row["productnaam"] . '</h5>
+    <p class="card-text">' . $row["productomschrijving"] . '</p>
+    <p class="card-text">€'  . $row["productprijs"] . '</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>';
+        // echo "<div class='row'><div class='col-6'>";
+        // echo "<p> Productnaam: " . $row["productnaam"] . "</p>";
+        // echo "<p> Productomschrijving: " . $row["productomschrijving"] . "</p>";
+        // echo "<p> Productprijs: " . $row["productprijs"] . "</p>";
+        // echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['productfoto'] ).'"/>';
       }
     }
   
